f0a5a205386dc0611a5c37c000824a0dcdc00eee
jorboare <jordi.b.arevalo@gmail.com>
2021-02-10 18:38:42 +0100

    terminado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Formulario from './components/Formulario'
 import Cancion from './components/Cancion'
+import Info from './components/Info'
 
 function App() {
 
   const [busquedaletra, guardarBusquedaLetra] = useState({})
   const [letra, guardarLetra] = useState('')
+  const [info, guardarInfo] = useState({})
 
 
   useEffect(() => {
@@ -17,14 +19,20 @@ function App() {
 
       const { artista, cancion } = busquedaletra
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`
 
-      const resultado = await axios(url)
+      const [letra, informacion] = await Promise.all([
+        axios(url),
+        axios(url2)
+      ])
 
-      guardarLetra(resultado.data.lyrics)
+      guardarLetra(letra.data.lyrics)
+      guardarInfo(informacion.data.artists[0])
+
     }
 
     consultarApiLetra()
-  }, [busquedaletra])
+  }, [busquedaletra, info])
 
   return (
     <>
@@ -35,6 +43,9 @@ function App() {
       <div className='container mt-5'>
         <div className='row'>
           <div className='col-md-6'>
+            <Info
+              info={info}
+            />
 
           </div>
           <div className='col-md-6'>
