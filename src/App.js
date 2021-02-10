de2308f43a092b8b2d38f9554dc38b65adbd10b2
jorboare <jordi.b.arevalo@gmail.com>
2021-02-10 13:02:53 +0100

    mostrar letra canción

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Formulario from './components/Formulario'
+import Cancion from './components/Cancion'
 
 function App() {
 
@@ -30,6 +31,19 @@ function App() {
       <Formulario
         guardarBusquedaLetra={guardarBusquedaLetra}
       />
+
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-md-6'>
+
+          </div>
+          <div className='col-md-6'>
+            <Cancion
+              letra={letra}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
